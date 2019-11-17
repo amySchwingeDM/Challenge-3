a918334365e9ea2154a0c2e236f49fc557af2510
amySchwingeDM <54595377+amySchwingeDM@users.noreply.github.com>
2019-11-17 16:33:36 -0500

    Add files via upload

---
+++ EvasiveAction.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+
+public class EvasiveAction : MonoBehaviour
+{
+    public float dodge;
+    public float smoothing;
+    public float tilt;
+    public Vector2 startWait;
+    public Vector2 actionTime;
+    public Vector2 actionWait;
+    public Boundary boundary;
+
+    private float currentSpeed;
+    private float targetAction;
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody> ();
+        currentSpeed = rb.velocity.z;
+        StartCoroutine(Evade());
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float newAction = Mathf.MoveTowards (rb.velocity.x, targetAction, Time.deltaTime * smoothing);
+        rb.velocity = new Vector3(newAction, 0.0f, currentSpeed);
+        rb.position = new Vector3
+        (
+            Mathf.Clamp (rb.position.x, boundary.xMin, boundary.xMax),
+            0.0f,
+            Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)
+        );
+
+        rb.rotation = Quaternion.Euler(0.0f, 0.0f, rb.velocity.x * -tilt);
+    }
+
+    IEnumerator Evade()
+    {
+        yield return new WaitForSeconds(Random.Range(startWait.x, startWait.y));
+
+        while (true)
+        {
+            targetAction = Random.Range(1, dodge) * -Mathf.Sign (transform.position.x);
+            yield return new WaitForSeconds(Random.Range (actionTime.x, actionTime.y));
+            targetAction = 0;
+            yield return new WaitForSeconds(Random.Range(actionWait.x, actionWait.y));
+        }
+    }
+}
---
+++ GameController.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject[] hazards;
+    public Vector3 spawnValues;
+    public int hazardCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+
+    public Text ScoreText;
+    public Text GameOverText;
+    public Text WinText;
+
+    private bool gameOver;
+    private bool restart;
+    private int score;
+
+    void Start()
+    {
+        gameOver = false;
+        restart = false;
+        GameOverText.text = "";
+        score = 0;
+        score = 0;
+        UpdateScore();
+        StartCoroutine(SpawnWaves());
+    }
+
+    void Update()
+    {
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+        if (restart == true)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                SceneManager.LoadScene("SampleScene");
+            }
+        }
+    }
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            for (int i = 0; i < hazardCount; i++)
+            {
+                GameObject hazard = hazards[Random.Range (0,hazards.Length)];
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+
+            if (gameOver == true)
+            {
+                GameOverText.text = "Press the space bar to Restart";
+                restart = true;
+                break;
+            }
+        }
+    }
+
+    public void AddScore(int newPointValue)
+    {
+        score += newPointValue;
+        UpdateScore();
+    }
+
+    void UpdateScore()
+    {
+        ScoreText.text = "Points: " + score;
+        if (score >= 100)
+        {
+            WinText.text = "You win! Game created by Amy Schwinge";
+            gameOver = true;
+            restart = true;
+        }
+    }
+    public void GameOver()
+    {
+        GameOverText.text = "Game Over!";
+        gameOver = true;
+    }
+}
